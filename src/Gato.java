61f95eca5032e283e0bed4950ee5e568dbd1092c
dgutper960 <115797153+dgutper960@users.noreply.github.com>
2022-11-23 10:10:48 +0100

    Clase Repaso Class

--- src/Gato.java
+++ src/Gato.java
@@ -1,10 +1,18 @@
+import jdk.dynalink.beans.StaticClass;
+
 public class Gato {
     private String nombre;
     private static String raza;
 
+
     Gato(String nombre){
         this.nombre = nombre;
     }
+    Gato(String nombre, String raza){
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+
 
     String getNombre(){
         return nombre;
@@ -13,6 +21,16 @@ public class Gato {
     void setNombre(String nombre){
         this.nombre = nombre;
     }
+
+
+    String getRaza(){
+        return raza;
+    }
+
+    void setRaza(String raza){
+        this.raza = raza;
+    }
+
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,16 @@ public class Main {
         System.out.println("Hello Word");
         saludar( "David");
 
-        Gato misifu = new Gato("Misifu");
+        Gato misifu = new Gato("Misifu", "Gato Montes");
         Gato isidoro = new Gato( "Isidoro");
         Gato sonia = new Gato("Sonia");
 
         System.out.println("Mi gato se llama: " + sonia.getNombre());
-        System.out.println("Mi gato se llama: " + misifu.getNombre());
+        System.out.println("Mi gato se llama: " + misifu.getNombre()  + " y es de la raza " + misifu.getRaza());
         System.out.println("Mi gato se llama: " + isidoro.getNombre());
         //Le cambiamos el nombre al gato Misifu
         misifu.setNombre("Bolita");
+        misifu.setRaza("");
         System.out.println("Mi gato ahora se llama: " + misifu.getNombre());
 
     }
