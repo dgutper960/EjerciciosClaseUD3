867885ed91f21c6a430953f1401aabb2f1ee9007
dgutper960 <115797153+dgutper960@users.noreply.github.com>
2022-11-23 09:43:50 +0100

    Clase Repaso Class

---
+++ src/Gato.java
@@ -0,0 +1,18 @@
+public class Gato {
+    private String nombre;
+    private static String raza;
+
+    Gato(String nombre){
+        this.nombre = nombre;
+    }
+
+    String getNombre(){
+        return nombre;
+    }
+
+    void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Word");
+        saludar( "David");
+
+        Gato misifu = new Gato("Misifu");
+        Gato isidoro = new Gato( "Isidoro");
+        Gato sonia = new Gato("Sonia");
+
+        System.out.println("Mi gato se llama: " + sonia.getNombre());
+        System.out.println("Mi gato se llama: " + misifu.getNombre());
+        System.out.println("Mi gato se llama: " + isidoro.getNombre());
+        //Le cambiamos el nombre al gato Misifu
+        misifu.setNombre("Bolita");
+        System.out.println("Mi gato ahora se llama: " + misifu.getNombre());
+
     }
+
+
+    public static void saludar(String nombre){
+        System.out.println("Hola " + nombre);
+    }
+
+
+
 }
